024f47b9990689f340115f5bbb54173da8ead05c
Bruno Alves <alves.bruno945@gmail.com>
2024-05-23 20:11:55 -0300

    Add `HomeViewController`

---
+++ NavigationTests/Scenes/HomeViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
