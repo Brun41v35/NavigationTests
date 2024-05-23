b298f12c14d6bf293a80bb52f1b99f798b143e86
Bruno Alves <alves.bruno945@gmail.com>
2024-05-23 20:54:22 -0300

    Add layout

--- NavigationTests/Scenes/Home/HomeViewController.swift
+++ NavigationTests/Scenes/Home/HomeViewController.swift
@@ -2,10 +2,89 @@ import UIKit
 
 final class HomeViewController: UIViewController {
 
+    // MARK: Private Properties
+
+    private let containerStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 12
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    private let pushViewControllerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Push ViewController", for: .normal)
+        button.configuration = .tinted()
+        button.configuration?.baseBackgroundColor = .systemBlue
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private let presentViewControllerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Present ViewController", for: .normal)
+        button.configuration = .tinted()
+        button.configuration?.baseBackgroundColor = .systemRed
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     // MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setup()
+    }
+
+    // MARK: - Private Methods
+
+    private func setup() {
+        setupViewHierarchy()
+        setupConstraints()
+        setupBackgroundColor()
+        bindLayoutEvents()
+    }
+
+    private func setupViewHierarchy() {
+        view.addSubview(containerStackView)
+        containerStackView.addArrangedSubview(pushViewControllerButton)
+        containerStackView.addArrangedSubview(presentViewControllerButton)
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            containerStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            pushViewControllerButton.heightAnchor.constraint(equalToConstant: 48),
+            presentViewControllerButton.heightAnchor.constraint(equalToConstant: 48)
+        ])
+    }
+
+    private func setupBackgroundColor() {
+        view.backgroundColor = .systemBackground
+    }
+
+    private func bindLayoutEvents() {
+        pushViewControllerButton.addTarget(self,
+                                           action: #selector(didTapPushViewControllerButton),
+                                           for: .touchUpInside)
+
+        presentViewControllerButton.addTarget(self,
+                                              action: #selector(didTapPresentViewControllerButton),
+                                              for: .touchUpInside)
+    }
+
+    @objc
+    private func didTapPushViewControllerButton() {
+        print("Push")
+    }
+
+    @objc
+    private func didTapPresentViewControllerButton() {
+        print("Present")
     }
 }
