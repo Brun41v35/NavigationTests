4db0ca6c3ade3a2a96efb88a06c3d84092b18b72
Bruno Alves <alves.bruno945@gmail.com>
2024-05-23 21:18:06 -0300

    Add layout on `ProfileViewController`

--- NavigationTests/Scenes/Profile/ProfileViewController.swift
+++ NavigationTests/Scenes/Profile/ProfileViewController.swift
@@ -2,9 +2,53 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
 
+    // MARK: Private Properties
+
+    private let textLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    // MARK: - Init
+
+    init(text: String) {
+        textLabel.text = text
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     // MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+
+    // MARK: - Private Methods
+
+    private func setup() {
+        setupViewHierarchy()
+        setupConstraints()
+        setupBackgroundColor()
+    }
+
+    private func setupViewHierarchy() {
+        view.addSubview(textLabel)
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            textLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+    private func setupBackgroundColor() {
+        view.backgroundColor = .systemBackground
     }
 }
