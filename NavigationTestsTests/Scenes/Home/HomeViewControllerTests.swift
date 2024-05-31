163d9e91868441c7a9021ee0148c9ae39a97350d
Bruno Alves <alves.bruno945@gmail.com>
2024-05-31 19:22:26 -0300

    Remove mock navigation

--- NavigationTestsTests/Scenes/Home/HomeViewControllerTests.swift
+++ NavigationTestsTests/Scenes/Home/HomeViewControllerTests.swift
@@ -2,30 +2,6 @@ import Foundation
 import XCTest
 @testable import NavigationTests
 
-final class NavigationControllerMock: UINavigationController {
-
-    // MARK: - Private(set) properties
-
-    private(set) var viewControllerArgs = [UIViewController]()
-    private(set) var viewControllerPresentArgs = [UIViewController]()
-    private(set) var pushViewControllerArgsAnimated = [Bool]()
-    private(set) var presentViewControllerArgsAnimated = [Bool]()
-
-    // MARK: - Methods
-
-    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
-        viewControllerArgs.append(viewController)
-        pushViewControllerArgsAnimated.append(animated)
-    }
-
-    override func present(_ viewControllerToPresent: UIViewController,
-                          animated flag: Bool,
-                          completion: (() -> Void)? = nil) {
-        viewControllerPresentArgs.append(viewControllerToPresent)
-        presentViewControllerArgsAnimated.append(flag)
-    }
-}
-
 final class HomeViewControllerTests: XCTestCase {
 
     func test_tappingPushButton_shouldPushProfileViewController() {
