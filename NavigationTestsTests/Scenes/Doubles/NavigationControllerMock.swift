dab704ae6a783755afdadc9b179e4d50d84c94c6
Bruno Alves <alves.bruno945@gmail.com>
2024-06-03 19:09:02 -0300

    Remove `override func present`

--- NavigationTestsTests/Scenes/Doubles/NavigationControllerMock.swift
+++ NavigationTestsTests/Scenes/Doubles/NavigationControllerMock.swift
@@ -6,9 +6,7 @@ final class NavigationControllerMock: UINavigationController {
     // MARK: - Private(set) properties
 
     private(set) var viewControllerArgs = [UIViewController]()
-    private(set) var viewControllerPresentArgs = [UIViewController]()
     private(set) var pushViewControllerArgsAnimated = [Bool]()
-    private(set) var presentViewControllerArgsAnimated = [Bool]()
 
     // MARK: - Methods
 
@@ -16,13 +14,5 @@ final class NavigationControllerMock: UINavigationController {
         super.pushViewController(viewController, animated: animated)
         viewControllerArgs.append(viewController)
         pushViewControllerArgsAnimated.append(animated)
-    }
-
-    override func present(_ viewControllerToPresent: UIViewController,
-                          animated flag: Bool,
-                          completion: (() -> Void)? = nil) {
-        super.present(viewControllerToPresent, animated: flag, completion: completion)
-        viewControllerPresentArgs.append(viewControllerToPresent)
-        presentViewControllerArgsAnimated.append(flag)
     }
 }
