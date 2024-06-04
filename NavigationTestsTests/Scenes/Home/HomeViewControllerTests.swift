dccd3e86f56ff1e92fe7cf984413159cade0c294
Bruno Alves <alves.bruno945@gmail.com>
2024-06-04 12:27:03 -0300

    Add `HomeViewControllerSpy` and call `tap` button method

--- NavigationTestsTests/Scenes/Home/HomeViewControllerTests.swift
+++ NavigationTestsTests/Scenes/Home/HomeViewControllerTests.swift
@@ -24,7 +24,8 @@ final class HomeViewControllerTests: XCTestCase {
     func test_tappingPresentButton_shouldPresentProfileViewController() {
         let sut = HomeViewController()
         sut.loadViewIfNeeded()
-        sut.presentViewControllerButton.sendActions(for: .touchUpInside)
+
+        tap(sut.presentViewControllerButton)
     }
 
     // MARK: - Helpers
@@ -38,5 +39,26 @@ final class HomeViewControllerTests: XCTestCase {
 
     private func tap(_ button: UIButton) {
         button.sendActions(for: .touchUpInside)
+    }
+}
+
+final class HomeViewControllerSpy: UIViewController {
+
+    // MARK: - Private(set) Properties
+
+    private(set) var presentCallCount = 0
+    private(set) var viewControllerToPresentArgs = [UIViewController]()
+    private(set) var animatedFlagArgs = [Bool]()
+    private(set) var completionArgs = [(() -> Void)?]()
+
+    // MARK: - Methods
+
+    override func present(_ viewControllerToPresent: UIViewController,
+                          animated flag: Bool,
+                          completion: (() -> Void)? = nil) {
+        presentCallCount += 1
+        viewControllerToPresentArgs.append(viewControllerToPresent)
+        animatedFlagArgs.append(flag)
+        completionArgs.append(completion)
     }
 }
